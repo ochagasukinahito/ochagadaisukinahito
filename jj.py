85a0c8f38dda430e200f07a1c8f3faff57aca7c1
Qota6 <87456383+Qota6@users.noreply.github.com>
2021-07-15 12:22:00 +0800

    Update jj.py
    出力の部分変更

--- jj.py
+++ jj.py
@@ -1,8 +1,8 @@
 print("Hello,World!")
 unko=int(input())
 if unko >=8 :
-  print(majiyabai)
+  print('majiyabai')
 elif unko >= 5 and unko <=7:
-  print(yabai)
+  print('yabai')
 elif unko >=0 and unko <= 4:
-  print(daijoubu)
+  print('daijoubu')
